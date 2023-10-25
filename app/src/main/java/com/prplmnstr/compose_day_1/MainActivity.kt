7344858aadc05b23dc787e37769705ede0f4d554
Santosh <santoshinchalakaranje@gmail.com>
2023-10-25 20:59:12 +0530

    day 2 _ rows , columsn and basic sizing

--- app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
+++ app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
@@ -3,12 +3,19 @@ package com.prplmnstr.compose_day_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import com.prplmnstr.compose_day_1.ui.theme.Compose_Day_1Theme
 
@@ -22,7 +29,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Santosh")
+                    Row(//modifier = Modifier
+                        // .fillMaxSize(0.1f)
+                    ) {
+
+                        Text(
+                            text = "Day 2 - Rows,Columns & Basic sizing"
+                        )
+                    }
+                    Row(
+                        //  modifier = Modifier
+                        // .fillMaxSize(0.5f)
+                        //.background(Color.White),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Text(text = "Item 1")
+                        Text(text = "Item 2")
+                        Text(text = "Item 3")
+                    }
                 }
             }
         }
@@ -32,6 +57,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
+
         text = "Hello $name!",
         modifier = modifier
     )
@@ -41,6 +67,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Compose_Day_1Theme {
-        Greeting("Sagar")
+        Greeting("Everyone")
     }
 }
