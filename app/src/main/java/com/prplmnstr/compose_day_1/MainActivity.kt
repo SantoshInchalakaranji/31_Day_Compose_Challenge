32d5803a9c135ad6d62ca733e0b334ea182f3e72
Santosh <santoshinchalakaranje@gmail.com>
2023-10-27 21:18:05 +0530

    day 4 ImageCard

--- app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
+++ app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
@@ -3,8 +3,12 @@ package com.prplmnstr.compose_day_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,33 +16,115 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.prplmnstr.compose_day_1.ui.theme.Compose_Day_1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier
-                .padding(16.dp)
-                .background(Color.Gray)
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth()
-            ) {
-                Text(text = "Hello")
-                Spacer(modifier = Modifier
-                    .width(16.dp)
-                    .height(16.dp)
+
+            Column {
+                Row(
+                    modifier=Modifier.fillMaxWidth()
+                    //.background(Color.Gray),
+                    //  horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    var painter = painterResource(id = R.drawable.daredevil1)
+                    var title = "Daredevil Wallpaper"
+                    ImageCard(painter = painter, contentDescription = title, title = title)
+                    painter = painterResource(id = R.drawable.daredevil2)
+                    title = "Daredevil Wallpaper"
+                    ImageCard(painter = painter, contentDescription = title, title = title)
+                }
+                Row(
+                    modifier=Modifier.fillMaxWidth()
+                    //.background(Color.Gray),
+                    //  horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    var painter = painterResource(id = R.drawable.daredevil3)
+                    var title = "Daredevil Wallpaper"
+                    ImageCard(painter = painter, contentDescription = title, title = title)
+                    painter = painterResource(id = R.drawable.daredevil4)
+                    title = "Daredevil Wallpaper"
+                    ImageCard(painter = painter, contentDescription = title, title = title)
+                }
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription:String,
+    title:String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier  = modifier
+            .width(200.dp)
+            .padding(16.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
+        Box(
+            modifier = Modifier.height(300.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
                 )
-                Text(text = "Everyone")
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+
+                    )
+                )){
+
+            }
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(text = title,
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                )
             }
         }
     }
