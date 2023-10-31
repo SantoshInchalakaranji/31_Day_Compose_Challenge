c37fe57a2840d3f5a86d81f0d86300ca132147a1
Santosh <santoshinchalakaranje@gmail.com>
2023-10-31 21:19:54 +0530

    Day 6 _ state

--- app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
+++ app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
@@ -3,44 +3,51 @@ package com.prplmnstr.compose_day_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.prplmnstr.compose_day_1.ui.theme.Compose_Day_1Theme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Compose_Day_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Santosh")
-                }
-            }
+            ColorBox(
+                Modifier.fillMaxSize()
+            )
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    Compose_Day_1Theme {
-        Greeting("Sagar")
+fun ColorBox(modifier: Modifier = Modifier) {
+    val color = remember{
+        mutableStateOf(Color.Yellow)
+    }
+
+    Box(modifier = modifier
+        .background(color.value)
+        .clickable {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+        }) {
+
     }
 }
