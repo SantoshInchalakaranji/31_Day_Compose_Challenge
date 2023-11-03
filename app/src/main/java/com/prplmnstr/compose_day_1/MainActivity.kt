742b3911a7356f597f177773e549e7d9ed5cad16
Santosh <santoshinchalakaranje@gmail.com>
2023-11-03 21:24:07 +0530

    day 7 snackbar , button , textfield

--- app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
+++ app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
@@ -3,44 +3,106 @@ package com.prplmnstr.compose_day_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.prplmnstr.compose_day_1.ui.theme.Compose_Day_1Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Compose_Day_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Santosh")
-                }
-            }
+
+
+
+            val scope = rememberCoroutineScope()
+
+
+
+            SnackbarM3()
+
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SnackbarM3() {
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                OutlinedTextField(
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Compose_Day_1Theme {
-        Greeting("Sagar")
+                    value = textFieldState, onValueChange = {
+
+                        textFieldState= it
+                })
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        scope.launch {
+                            val result = snackbarHostState.showSnackbar(
+                                message = textFieldState,
+                                actionLabel = "Ok",
+                                withDismissAction = true,
+                                duration = SnackbarDuration.Long
+                            )
+                            when (result) {
+                                SnackbarResult.Dismissed -> {
+
+                                }
+
+                                SnackbarResult.ActionPerformed -> {
+                                    /* viewModel.sendPhoto*/
+                                }
+                            }
+                        }
+                    }
+                ) {
+                    Text(text = "Show Snackbar")
+                }
+            }
+        }
+
     }
 }
+
+
