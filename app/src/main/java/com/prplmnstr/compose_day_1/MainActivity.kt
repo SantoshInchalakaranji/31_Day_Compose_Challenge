609eb1630e28e5c1a3179126bb2ef7c24009f587
Santosh <santoshinchalakaranje@gmail.com>
2023-11-04 12:17:12 +0530

    day 9 ConstraintLayout

--- app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
+++ app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
@@ -3,44 +3,75 @@ package com.prplmnstr.compose_day_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.prplmnstr.compose_day_1.ui.theme.Compose_Day_1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Compose_Day_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Santosh")
+            val constraints = ConstraintSet {
+                val redBox = createRefFor("redBox")
+                val yellowBox = createRefFor("yellowBox")
+
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.matchParent
+                    height = Dimension.value(100.dp)
+
+                }
+                constrain(yellowBox) {
+                    top.linkTo(redBox.bottom)
+                    start.linkTo(parent.start)
+                    width = Dimension.matchParent
+                    height = Dimension.value(100.dp)
+
                 }
             }
+
+            ConstraintLayout(
+                constraints,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+
+            ) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Yellow)
+                        .layoutId("yellowBox")
+
+                )
+                Box(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .layoutId("redBox")
+
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Compose_Day_1Theme {
-        Greeting("Sagar")
     }
 }
