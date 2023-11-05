e1b880204d4885ef510221e4c99b49c7282d2b6e
Santosh <santoshinchalakaranje@gmail.com>
2023-11-05 11:55:40 +0530

    day 12 progress animation

--- app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
+++ app/src/main/java/com/prplmnstr/compose_day_1/MainActivity.kt
@@ -3,44 +3,99 @@ package com.prplmnstr.compose_day_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.prplmnstr.compose_day_1.ui.theme.Compose_Day_1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Compose_Day_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Santosh")
+            Box (
+
+                contentAlignment = Alignment. Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Row {
+                    CircularProgressBar(percentage = 0.8f, number = 100)
+                    Spacer(modifier = Modifier.width(32.dp))
+                    CircularProgressBar(percentage = 0.7f, number = 100, color = Color.DarkGray)
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CircularProgressBar (
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(  false)
+    }
+    val curPercentage = animateFloatAsState (
+        targetValue = if(animationPlayed) percentage else 0f,
+        animationSpec = tween(
+                durationMillis = animDuration,
+        delayMillis = animDelay
     )
-}
+    )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Compose_Day_1Theme {
-        Greeting("Sagar")
+    LaunchedEffect (key1 = true) {
+        animationPlayed = true
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size( radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+             360 * curPercentage.value,
+            useCenter = false,
+            style = Stroke(strokeWidth.toPx(), cap = StrokeCap. Round)
+            )
+        }
+        Text(
+            text = (curPercentage.value *number).toInt().toString(),
+        color= Color.Black,
+        fontSize = fontSize,
+        fontWeight = FontWeight. Bold
+        )
     }
 }
